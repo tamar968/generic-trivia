deaddfe327615e5c0e40a46a939b94261a8a4efe
tamar <tamar96880@gmail.com>
2021-01-25 12:47:09 +0200

    init generic trivia game

---
+++ js/JavaScript.js
@@ -0,0 +1,151 @@
+﻿
+//my code
+var questions = userQuestions;
+var questionIndex = 0;
+var question = document.getElementById("question");
+var answer1 = document.getElementById("ans1Content");
+var answer2 = document.getElementById("ans2Content");
+var answer3 = document.getElementById("ans3Content");
+var pointsGroup1 = 0;
+var pointsGroup2 = 0;
+var userTimeUntilMusicStop = timeUntilMusicStop;
+var userTimeUntilViewAnswer = timeUntilViewAnswer;
+var mus = document.getElementById("music");
+var dinor = document.getElementById("dinor");
+
+function startGame() {
+    document.getElementById('allGameScreens').style.display = 'block';
+    nextQustion();
+}
+function nextQustion() {
+
+    if (questionIndex == questions.length) {
+        win();
+        return;
+    }
+    mus.load();
+    mus.play();
+
+    //change the content of the question and the answers
+    question.innerHTML = questions[questionIndex].question;
+    answer1.innerHTML = questions[questionIndex].answer1;
+    answer2.innerHTML = questions[questionIndex].answer2;
+    answer3.innerHTML = questions[questionIndex].answer3;
+
+    var anim = "bounceInUp";
+    testAnim(anim);
+
+    //reset the old value answer
+    answer1.value = 0;
+    answer2.value = 0;
+    answer3.value = 0;
+
+    questionIndex++;
+    pb = 0;
+    moveProgress(userTimeUntilMusicStop);
+}
+function viewCorrectAnswer() {
+    mus.pause();
+    clearInterval(progressInterval);
+    document.getElementById('id01').style.display = 'block';
+    //alert("viewCorrectAnswer" + questions[questionIndex - 1].correctAnswer);
+    var correct = document.getElementById("correctAnswer");
+    switch (questions[questionIndex - 1].correctAnswer) {
+        case 1: correct.innerHTML = questions[questionIndex - 1].answer1;
+            break;
+        case 2: correct.innerHTML = questions[questionIndex - 1].answer2;
+            break;
+        case 3: correct.innerHTML = questions[questionIndex - 1].answer3;
+            break;
+        default: correct.innerHTML = "תשובה 4";
+    }
+}
+function win() {
+    mus.load();
+    mus.play();
+    var winScreen = document.getElementById("winScreen");
+    var winTitle = document.getElementById("winTitle");
+    var winGruop = document.getElementById("winGroup");
+    var winPoints = document.getElementById("winPoints");
+    winTitle.innerHTML = 'הקבוצה המנצחת היא: '
+    if (pointsGroup1 > pointsGroup2) {
+        winGruop.innerHTML = 'קבוצה א';
+        winPoints.innerHTML = pointsGroup1;
+    } else if (pointsGroup1 < pointsGroup2) {
+        winGruop.innerHTML = 'קבוצה ב';
+        winPoints.innerHTML = pointsGroup2;
+    }
+    else {
+        winTitle.innerHTML = 'מספר נקודות זהה ל-2 הקבוצות';
+        winGruop.innerHTML = 'לכל קבוצה';
+        winPoints.innerHTML = pointsGroup1;
+    }
+    winScreen.style.display = 'block';
+    winPoints.innerHTML += ' נקודות';
+    playGameOverAnimation(2);
+    setInterval(function () { playGameOverAnimation(2); }, 3000);
+}
+
+function playGameOverAnimation(numOfCanvas) {
+    for (let i = 1; i <= numOfCanvas; i++) {
+        setTimeout(function () { gameOver("#canvas" + i);},1500);
+    }
+}
+
+
+
+//my stopper
+var sec = userTimeUntilMusicStop;
+var ms = 0;
+var secElem = document.getElementById("sec");
+var msElem = document.getElementById("ms");
+
+function stopper(time, func) {
+    sec = time;
+    secElem.innerHTML = sec < 10 ? ("0" + sec) : sec;
+    var interval = setInterval(function () {
+
+        if (sec == 0 && ms == 0) {
+            clearInterval(interval);
+            if (func)
+                func();
+            return;
+        }
+
+        if (ms == 0) {
+            msElem.innerHTML = ms = 100;
+            if (sec > 0) {
+                secElem.innerHTML = (--sec < 10) ? ("0" + sec) : sec;
+            }
+        }
+        msElem.innerHTML = (--ms < 10) ? ("0" + ms) : ms;
+    }, 10);
+}
+
+var pb = 0;
+var width = 100;
+var progressInterval;
+function moveProgress(timeInSec, eachTime = 0.01, callback = viewCorrectAnswer) {
+    if (pb == 0) {
+        pb = 1;
+        var elem = document.getElementById('myBar');
+        var width = 1;
+        progressInterval = setInterval(frame, eachTime * 1000);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(progressInterval);
+                pb = 0;
+                if (callback) {
+                    callback();
+                }
+            } else {
+                width += eachTime * 100 / timeInSec;
+                elem.style.width = width + '%';
+            }
+        }
+    }
+}
+
+function cssAnim(elem, anim) {
+    elem.classList.add(anim);
+}
---
+++ js/data.js
@@ -0,0 +1,90 @@
+﻿var timeUntilMusicStop = 20;
+var timeUntilViewAnswer = 0;
+var scoreForEachTime = 50;
+
+//question: "כאן כתבי את השאלה",
+//answer1: " תשובה1",
+//answer2: " תשובה2",
+//answer3: " תשובה3",
+//correctAnswer: כאן כתבי את מספר התשובה הנכונה
+
+
+var userQuestions = [
+    {
+        question: 'באיזו שנה הוקמה עמותת "גניזה כללית"?',
+        answer1:'1.	תש"ע',
+        answer2: '2. תשמ"ו',
+        answer3: '3. תשכ"ו',
+        correctAnswer: 2
+    },
+    {
+        question: 'באיזו שכונה הוצב מתקן הגניזה הראשון בעולם?',
+        answer1: '1. מטרסדורף',
+        answer2: '2. נוה יעקב',
+        answer3:'3. בית וגן',
+        correctAnswer: 3
+    },
+    {
+        question: 'כל כמה זמן מפנים גניזה?',
+        answer1: '1. מידי יום',
+        answer2: '2. פעם בשבוע',
+        answer3:'3. פעם בחודש',
+        correctAnswer: 1
+    },
+    {
+        question: 'מדוע אסור לחטט במתקן הגניזה?',
+        answer1: '1. כי יש בפנים דברי קודש',
+        answer2: '2. כי כך הגניזה נשמרת באדמה יותר בכבוד',
+        answer3: '3. 2 התשובות נכונות',
+        correctAnswer: 3
+    },
+    {
+        question: 'ממתי קיים המושג "גניזה"?',
+        answer1: '1. מאז המצאת הדפוס',
+        answer2: '2. מאז ומעולם',
+        answer3: '3. בעשורים האחרונים',
+        correctAnswer: 2
+    },
+    {
+        question: 'האם טלית טעונה גניזה?',
+        answer1: '1. כן, גניזה חמורה',
+        answer2: '2. כן, גניזה רגילה',
+        answer3: '3. לא',
+        correctAnswer: 2
+    },
+    {
+        question: 'איזו גניזה טעונה מזוזה?',
+        answer1: '1. גניזה רגילה',
+        answer2: '2. גניזה חמורה',
+        answer3: '3. זה רק חומרא לגנוז ',
+        correctAnswer: 2
+    },
+    {
+        question: 'האם כיפה טעונה גניזה?',
+        answer1: '1. כן',
+        answer2: '2. לא',
+        answer3: '3. תלוי אם השתמשו בה או לא',
+        correctAnswer: 2
+    },
+    {
+        question: 'באלו כדים נקברת הגניזה החמורה בזמננו?',
+        answer1: '1. כדי חרס',
+        answer2: '2. כדי פלסטיק',
+        answer3: '3. כדי ברזל',
+        correctAnswer: 2
+    },
+    {
+        question: 'היכן קוברים גניזה חמורה?',
+        answer1: '1. בבור עמוק באדמה',
+        answer2: '2. בבית קברות',
+        answer3: '3. בתוך קבר של תלמיד חכם',
+        correctAnswer:2
+    },
+    {
+        question: 'כמה מתקני גניזה יש בירושלים?',
+        answer1: '1. 50',
+        answer2: '2. 70',
+        answer3: '3. למעלה מ-100',
+        correctAnswer: 3
+    }
+]
